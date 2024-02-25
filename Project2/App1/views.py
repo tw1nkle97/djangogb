250222deb94e00ca3117ed9e9bf617cdaf1726f9
Ivan <grabovivan@gmail.com>
2024-02-25 17:40:42 +0300

    hw5 done in Project2 dir

--- Project2/App1/views.py
+++ Project2/App1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from datetime import timedelta
-from .models import Order, Product
+from .models import Client, Order, Product
 from django.db.models import Q
 from django.http import HttpResponse
 
@@ -19,3 +19,38 @@ def client_products(request, client_id, period_days):
     products_ids = orders.values_list('products', flat=True).distinct()
     products = Product.objects.filter(id__in=products_ids)
     return render(request, 'client_products.html', {'products': products, 'period_days': period_days})
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
+
+
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
+
+
+def client_detail(request, client_id):
+    client = Client.objects.get(id=client_id)
+    return render(request, 'client_detail.html', {'client': client})
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
+
+
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'order_list.html', {'orders': orders})
+
+
+def order_detail(request, order_id):
+    order = Order.objects.get(id=order_id)
+    return render(request, 'order_detail.html', {'order': order})
--- Project2/Project2/urls.py
+++ Project2/Project2/urls.py
@@ -23,4 +23,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('client/<int:client_id>/orders/', views.client_orders, name='client_orders'),
     path('client/<int:client_id>/products/<int:period_days>/', views.client_products, name='client_products'),
+    path('clients/', views.client_list, name='client_list'),
+    path('clients/<int:client_id>/', views.client_detail, name='client_detail'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('orders/', views.order_list, name='order_list'),
+    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+
 ]
